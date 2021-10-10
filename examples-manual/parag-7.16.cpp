6f0a60d79d8f31445e2f5ca814475bb21bbb80e2
Cristian Barbarosie <cristian.barbarosie@gmail.com>
2021-10-10 09:09:03 +0100

    21.10.09

--- examples-manual/parag-7.16.cpp
+++ examples-manual/parag-7.16.cpp
@@ -12,22 +12,22 @@ int main ( )
 	Function xy = RR2.build_coordinate_system ( tag::Lagrange, tag::of_degree, 1 );
 	Function x = xy[0], y = xy[1];
 
-	size_t n = 10;
-	double d = 1.3 / double(n);
+	const double d = 0.13;
+	const size_t n = 1.5 / d, m = 2.6 / d;
 
-	Cell A ( tag::vertex );  x(A) = -1.3;  y(A) = -1.3;
+	Cell A ( tag::vertex );  x(A) = -1.5;  y(A) = -1.3;
 	Cell B ( tag::vertex );  x(B) =  0. ;  y(B) = -1.3;
-	Cell C ( tag::vertex );  x(C) =  1.3;  y(C) = -1.3;
-	Cell D ( tag::vertex );  x(D) =  1.3;  y(D) =  1.3;
+	Cell C ( tag::vertex );  x(C) =  1.5;  y(C) = -1.3;
+	Cell D ( tag::vertex );  x(D) =  1.5;  y(D) =  1.3;
 	Cell E ( tag::vertex );  x(E) =  0. ;  y(E) =  1.3;
-	Cell F ( tag::vertex );  x(F) = -1.3;  y(F) =  1.3;
+	Cell F ( tag::vertex );  x(F) = -1.5;  y(F) =  1.3;
 
-	Mesh AB ( tag::segment, A.reverse(), B, tag::divided_in,   n );
-	Mesh BC ( tag::segment, B.reverse(), C, tag::divided_in,   n );
-	Mesh CD ( tag::segment, C.reverse(), D, tag::divided_in, 2*n );
-	Mesh DE ( tag::segment, D.reverse(), E, tag::divided_in,   n );
-	Mesh EF ( tag::segment, E.reverse(), F, tag::divided_in,   n );
-	Mesh FA ( tag::segment, F.reverse(), A, tag::divided_in, 2*n );
+	Mesh AB ( tag::segment, A.reverse(), B, tag::divided_in, n );
+	Mesh BC ( tag::segment, B.reverse(), C, tag::divided_in, n );
+	Mesh CD ( tag::segment, C.reverse(), D, tag::divided_in, m );
+	Mesh DE ( tag::segment, D.reverse(), E, tag::divided_in, n );
+	Mesh EF ( tag::segment, E.reverse(), F, tag::divided_in, n );
+	Mesh FA ( tag::segment, F.reverse(), A, tag::divided_in, m );
 
 	Manifold circle = RR2.implicit ( x*x + y*y == 1. );
 	Mesh inner ( tag::progressive, tag::entire_manifold, circle, tag::desired_length, d );
