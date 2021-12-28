a3097845394ea5f1b867680f499f585ade83c546
Cristian Barbarosie <cristian.barbarosie@gmail.com>
2021-12-28 14:41:39 +0000

    21.12.28a

--- examples-manual/parag-1.6.cpp
+++ examples-manual/parag-1.6.cpp
@@ -1,7 +1,7 @@
 
-// example presented in paragraph 1.5 of the manual
+// example presented in paragraph 1.6 of the manual
 // http://manifem.rd.ciencias.ulisboa.pt/manual-manifem.pdf
-// builds a trapezoidal mesh made of triangles
+// a mesh which mixes triangles and rectangles
 
 #include "maniFEM.h"
 
@@ -19,24 +19,26 @@ int main () {
 	// we can extract components of xy using the [] operator :
 	Function x = xy [0],  y = xy [1];
 
-	Cell A ( tag::vertex );  x (A) = -1. ;  y (A) = 0.;
-	Cell B ( tag::vertex );  x (B) =  0. ;  y (B) = 0.;
-	Cell C ( tag::vertex );  x (C) =  1. ;  y (C) = 0.;
-	Cell D ( tag::vertex );  x (D) = -0.5;  y (D) = 1.;
-	Cell E ( tag::vertex );  x (E) =  0.5;  y (E) = 1.;
+	Cell A ( tag::vertex );  x (A) = -1. ;  y (A) = 0. ;
+	Cell B ( tag::vertex );  x (B) =  0. ;  y (B) = 0. ;
+	Cell C ( tag::vertex );  x (C) =  1. ;  y (C) = 0. ;
+	Cell D ( tag::vertex );  x (D) = -0.5;  y (D) = 0.8;
+	Cell E ( tag::vertex );  x (E) =  0.5;  y (E) = 0.8;
+	Cell F ( tag::vertex );  x (F) =  0. ;  y (F) = 1. ;
 	Mesh AB ( tag::segment, A .reverse(), B, tag::divided_in, 8 );
 	Mesh BC ( tag::segment, B .reverse(), C, tag::divided_in, 8 );
 	Mesh AD ( tag::segment, A .reverse(), D, tag::divided_in, 8 );
 	Mesh BD ( tag::segment, B .reverse(), D, tag::divided_in, 8 );
 	Mesh BE ( tag::segment, B .reverse(), E, tag::divided_in, 8 );
 	Mesh CE ( tag::segment, C .reverse(), E, tag::divided_in, 8 );
-	Mesh ED ( tag::segment, E .reverse(), D, tag::divided_in, 8 );
+	Mesh EF ( tag::segment, E .reverse(), F, tag::divided_in, 8 );
+	Mesh FD ( tag::segment, F .reverse(), D, tag::divided_in, 8 );
 	Mesh ABD ( tag::triangle, AB, BD, AD .reverse() );
 	Mesh BCE ( tag::triangle, BC, CE, BE .reverse() );
-	Mesh BED ( tag::triangle, BE, ED, BD .reverse() );
-	Mesh three_tri ( tag::join, ABD, BCE, BED );
+	Mesh BEFD ( tag::rectangle, BE, EF, FD, BD .reverse() );
+	Mesh two_tri_one_rect ( tag::join, ABD, BEFD, BCE );
 
-	three_tri .export_msh ("three-tri.msh");
+	two_tri_one_rect .export_msh ("two-tri-one-rect.msh");
 	
-	cout << "produced file three-tri.msh" << endl;
+	cout << "produced file two-tri-one-rect.msh" << endl;
 }
